80dfb6bccf278f243360a1aa7ee79fa7fccf0b6c
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-26 10:11:07 -0700

    Testing app enlight

--- albionmarket_backend/app.py
+++ albionmarket_backend/app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 from flask_restful import Api
+from appenlight_client import client as e_client
+from appenlight_client.ext import flask as appenlight
 
 from .resources import configure_resources
 from .extensions import configure_extensions
@@ -17,5 +19,8 @@ def create_app():
 
     configure_resources(api)
     configure_extensions(app)
+
+    config = e_client.get_config()
+    app = appenlight.add_appenlight(app, config)
 
     return app
