272cc28af6a518d7095412030b70261ca0bb7c87
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-26 12:18:21 -0700

    Adding resource that sumarizes all basic resources

--- albionmarket_backend/resources/__init__.py
+++ albionmarket_backend/resources/__init__.py
@@ -3,6 +3,7 @@
 from .ingest import IngsetV1
 from .orders import OrdersV1
 from .orders_stats import OrdersStatsV1
+from .orders_resources import OrdersResourcesV1
 from .items import ItemsV1
 from .categories import CategoriesV1
 from .stats import StatsV1
@@ -12,6 +13,7 @@ def configure_resources(api):
     api.add_resource(IngsetV1, '/api/v1/ingest/')
     api.add_resource(OrdersV1, '/api/v1/orders/<string:item_id>/')
     api.add_resource(OrdersStatsV1, '/api/v1/orders/<string:item_id>/stats/')
+    api.add_resource(OrdersResourcesV1, '/api/v1/orders/resources/')
     api.add_resource(ItemsV1, '/api/v1/items/')
     api.add_resource(CategoriesV1, '/api/v1/categories/')
     api.add_resource(StatsV1, '/api/v1/stats/')
---
+++ albionmarket_backend/resources/orders_resources.py
@@ -0,0 +1,36 @@
+
+
+from flask_restful import Resource
+from sqlalchemy import not_
+
+from .orders_stats import fetch_item_market_stats
+from ..models import Item
+
+
+class OrdersResourcesV1(Resource):
+    def get(self):
+        resources = Item.query.filter(not_(Item.id.like('%_LEVEL%'))).filter_by(category_id='resources')
+
+        results = []
+
+        for item in resources:
+            stats = fetch_item_market_stats(item.id)
+
+            results.append({
+                'stats': stats,
+                'item': {
+                    'id': item.id,
+                    'name': item.name,
+                    'category_id': item.category_id,
+                    'category_name': item.category.name,
+                    'sub_category_id': item.sub_category_id,
+                    'sub_category_name': item.sub_category.name,
+                    'tier': item.tier,
+                },
+            })
+
+        data = {
+            'resources': results
+        }
+
+        return data, 200
