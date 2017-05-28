f91d1f4bbfc76c429b7981c82521cc2aaa091dc5
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-28 09:16:40 -0700

    Adding Sentry

--- albionmarket_backend/extensions.py
+++ albionmarket_backend/extensions.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
 from flask_cors import CORS
+from raven.contrib.flask import Sentry
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 cache = Cache()
 cors = CORS()
+sentry = Sentry()
 
 
 def configure_extensions(app):
@@ -18,3 +20,4 @@ def configure_extensions(app):
     migrate.init_app(app, db)
     cache.init_app(app)
     cors.init_app(app)
+    sentry.init_app(app)
