ee71635d7006a775ebcb1da62fb8438d29ecdb7a
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-28 09:31:31 -0700

    Fix item ID from ingest orders with enchantment levels

--- albionmarket_backend/resources/ingest.py
+++ albionmarket_backend/resources/ingest.py
@@ -21,10 +21,17 @@ class IngsetV1(Resource):
         for order in args['MarketItems']:
             order_json = json.loads(order)
 
+            # Even though the enchantment level is included elsewhere in the payload
+            # the item ID seems to include it as well in the form of item_id@enchantment_level.
+            # Cannot use this as an item ID as it fails the foreign key constraint. So split
+            # on the @ character and return just the first part. This does make the assumption
+            # that no item ID ever has an @ in it for other purposes... please don't hate me SI.
+            item_id = order_json['ItemTypeId'].split('@')[0]
+
             try:
                 MarketOrder.create_or_update(
                     order_id=order_json['Id'],
-                    item_id=order_json['ItemTypeId'],
+                    item_id=item_id,
                     location_id=args['LocationId'],
                     quality=order_json['QualityLevel'],
                     enchantment=order_json['EnchantmentLevel'],
